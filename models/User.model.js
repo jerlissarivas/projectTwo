5e1ae4eef5e1190cf75c6662ceb28bb2730f53f8
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2020-02-19 19:57:33 -0500

    Update user model

--- models/User.model.js
+++ models/User.model.js
@@ -17,7 +17,7 @@ const userSchema = new Schema(
       lowercase: true,
       trim: true
     },
-    passwordHash: {
+    password: {
       type: String,
       required: [true, 'Password is required.']
     }
