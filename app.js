96ff5a23bda64276140bc011eeec4bfdf993f262
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2020-02-19 19:54:03 -0500

    NPM installed express-sessions and connect-mongo

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 mongoose
   .connect('mongodb://localhost/project-two', {useCreateIndex: true,
@@ -50,6 +52,28 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // default value for title local
 app.locals.title = 'Module 2 - Project 2';
+
+app.use(
+  session({
+      secret: "regenerator",
+      resave: true,
+      saveUninitialized: true,
+      store: new MongoStore({ mongooseConnection: mongoose.connection })
+  })
+);
+
+app.use((req, res, next) => {
+  // here I will set a local variable for the body class. This is just another way that you can use local variables and for our purposes we will be doing this in order to track when we are in message boards details page
+  res.locals.bodyClass = "default";
+
+  // thanks to passport we have access to req.session.user to get the current users information. By using res.locals to set the variable currentUser we can now call currentUser from any of our view pages since we are declaring it in the app.js
+  // normally setting res.locals variable in a route will only give you access to that variable in the view page that you are rendering.
+  res.locals.currentUser = req.session.user;
+
+  // after we have finished what needs to be done in this section we call next so that we continue onto the next process that must be ran.
+  // ** forgetting to add a next() here can make your app hang and you may not get any error messages letting your know why it is hanging. If you see  GET / - - ms - -  in your terminal then that will more than likely mean that you forgot to add a next here.
+  next();
+});
 
 
 
