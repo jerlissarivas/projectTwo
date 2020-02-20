c6d1096d19c6c3e45154cc3bb88f65c764681277
Stanley Fils <stanleyfils@gmail.com>
2020-02-19 20:56:05 -0500

    Updated User model Schema

--- models/User.model.js
+++ models/User.model.js
@@ -1,30 +1,34 @@
 // models/User.model.js
 
-const { Schema, model } = require('mongoose');
+const {
+  Schema,
+  model
+} = require('mongoose');
 
-const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      trim: true,
-      required: [true, 'Username is required.'],
-      unique: true
-    },
-    email: {
-      type: String,
-      required: [true, 'Email is required.'],
-      unique: true,
-      lowercase: true,
-      trim: true
-    },
-    passwordHash: {
-      type: String,
-      required: [true, 'Password is required.']
-    }
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    trim: true,
+    required: [true, 'First name is required.'],
   },
-  {
-    timestamps: true
+  lastName: {
+    type: String,
+    trim: true,
+    required: [true, 'First name is required.'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required.'],
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  passwordHash: {
+    type: String,
+    required: [true, 'Password is required.']
   }
-);
+}, {
+  timestamps: true
+});
 
 module.exports = model('User', userSchema);
