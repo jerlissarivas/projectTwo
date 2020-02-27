77cba71d0c3f54379cc62a4ea8c9d1f5c1f1483d
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2020-02-26 22:32:07 -0500

    Add Configs folder. Create Passport subfolder and add google strategy file for use of google api. Create login option with gmail using passport.  Create db.config file. gst

---
+++ configs/passport/google-strategy.config.js
@@ -0,0 +1,43 @@
+const passport =require('passport');
+// const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const GoogleStrategy = require('passport-google-oauth20');
+
+const User = require('../../models/User.model')
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.googleClientId,
+  clientSecret: process.env.googleClientSecret,
+  callbackURL: '/google/callback',
+  proxy: true // important for production
+}, (accessToken, refreshToken, userInfo, next) => {
+  // console.log('Google acc: ', userInfo);
+
+  // use ES6 destructuring instead of having this:
+  // const displayName = userInfo.displayName;
+  const { id, name: { givenName, familyName }, emails } = userInfo;
+
+
+  // we don't want users to have double accounts in our app, that's why we will check if we already have
+  // user with the gmail he's using, and if yes, we will just log them in
+  User.findOne({ $or: [
+    { email: emails[0].value },
+    { googleID: userInfo.id } // => this step might be "over protection" of our app but you know ... 🤷‍♀️
+  ] })
+  .then( user => {
+    if(user){
+      next(null, user); // log in the user if the user already exists
+      return;
+    } 
+
+    // if we don't find the user in the DB, then create one
+    User.create({
+      email: emails[0].value,
+      firstName: givenName,
+      lastName: familyName,
+      googleID: id
+    })
+    .then( newUser => next(null, newUser) )
+    .catch( err => next(err) );
+  } )
+  .catch( err => next(err) );
+}))
