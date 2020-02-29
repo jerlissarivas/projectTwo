d14b982f8de96ef31358d2c56118b792dbc13782
Stanley Fils <stanleyfils@gmail.com>
2020-02-29 14:28:29 -0500

    Connect passport local

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
 
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 
-const passportSetup =  require('./configs/passport/index.config');
+const passportSetup = require('./configs/passport/index.config');
 
 // Set up the database
 require('./configs/db.config');
@@ -25,7 +25,9 @@ const app = express();
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 
 // Express View engine setup
@@ -35,7 +37,7 @@ app.use(cookieParser());
 //   dest: path.join(__dirname, 'public'),
 //   sourceMap: true
 // }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -49,15 +51,22 @@ app.locals.title = 'Module 2 - Project 2';
 
 app.use(
   session({
-      secret: "regenerator",
-      resave: true,
-      saveUninitialized: true,
-      store: new MongoStore({ mongooseConnection: mongoose.connection })
+    secret: "regenerator",
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection
+    })
   })
 );
 
 // 🎯🎯🎯 MUST come after the session: 🎯🎯🎯
 passportSetup(app);
+
+// app.use((req, res, next) => {
+//   res.locals.currentUser = req.session.user;
+//   next();
+// })
 
 const index = require('./routes/index');
 app.use('/', index);
