6b8318d800e99ad4534f63d0e7cde18b84245d51
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2020-03-01 15:41:42 -0500

    updated routes

--- app.js
+++ app.js
@@ -69,10 +69,10 @@ passportSetup(app);
 // })
 
 const index = require('./routes/index');
-app.use('/', index);
+// app.use('/', index);
 app.use('/', require('./routes/auth.routes'));
 app.use('/', require('./routes/contacts.routes'));
-// app.use('/', require('./routes/news.routes'));
+app.use('/', require('./routes/news.routes'));
 // app.use('/', require('./routes/calendar.routes'));
 
 
