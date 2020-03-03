26ba7c085759934775d7fee497191f3b9b18e3a6
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2020-03-02 19:37:28 -0500

    Add option to select groups

--- routes/contacts.routes.js
+++ routes/contacts.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router  = express.Router();
 
 const Contact = require('../models/Contact.model');
+const Group = require('../models/Group.model')
 
 // DELETE CONTACT
 
@@ -74,7 +75,15 @@ router.get('/contacts', (req, res, next) => {
 // Add Contacts
 
 router.get('/addcontact', (req, res, next) => {
-  res.render('contacts/new-contact');
+  Group.find()
+  .then(groupsFromDB => {
+    console.log(groupsFromDB)
+  res.render('contacts/new-contact', {groups: groupsFromDB});
+  })
+  .catch(err => {
+    console.log(`Error while getting groups from the DB: ${err}`);
+    next(err);
+  });
 });
 
 router.post('/addcontact', (req, res, next) => {
