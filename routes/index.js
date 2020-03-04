f7ead22fef838b7307ebaba3c60da43313412da7
Stanley Fils <stanleyfils@gmail.com>
2020-03-04 12:23:25 -0500

    Add Boostrap Navbar

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,9 @@
-const express = require('express');
-const router  = express.Router();
-const axios = require("axios");
-
+const express = require("express");
+const router = express.Router();
 
 /* GET home page */
-// router.get('/', (req, res, next) => {
-//   var url = 'http://newsapi.org/v2/everything?' +
-//          'q=Apple&' +
-//          'from=2020-02-29&' +
-//          'sortBy=popularity&' +
-//          'apiKey=54546ecca52e4e1698544474ecb7329e';
-//  axios.get(url)
-//  .then(data => {
-//     let articles = data.articles
-//     console.log(data.data.articles)
-//     res.render('index', {articles})
-//   })
-//   .catch(err => {
-//     console.log(err)
-//   })
-// });
-
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
 
 module.exports = router;
