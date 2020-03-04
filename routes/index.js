d89b81e256fcbb2985fe927430d0bf7f1a33e43b
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2020-03-04 12:54:04 -0500

    Clean up index route and view

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,3 @@
-// const express = require('express');
-// const router  = express.Router();
-// const axios = require("axios");
-
-
-/* GET home page */
-// router.get('/', (req, res, next) => {
-//   var url = 'http://newsapi.org/v2/everything?' +
-//          'q=Apple&' +
-//          'from=2020-02-29&' +
-//          'sortBy=popularity&' +
-//          'apiKey=54546ecca52e4e1698544474ecb7329e';
-//  axios.get(url)
-//  .then(data => {
-//     let articles = data.articles
-//     console.log(data.data.articles)
-//     res.render('index', {articles})
-//   })
-//   .catch(err => {
-//     console.log(err)
-//   })
-// });
-
 const express = require("express");
 const router = express.Router();
 
@@ -28,7 +5,5 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
     res.render("index");
 });
-
-module.exports = router;
 
 module.exports = router;
